864654b6eb53fd250bd604bc5a84f37c64b986be
sivaanand13 <99034823+sivaanand13@users.noreply.github.com>
2023-11-09 16:01:56 -0500

    Adding all interfaces and classes needed for backend.

---
+++ townService/src/town/games/ChessGame.ts
@@ -0,0 +1,78 @@
+import Player from '../../lib/Player';
+import { GameMove, ChessGameState, ChessMove } from '../../types/CoveyTownSocket';
+import Game from './Game';
+
+/**
+ * A ChessGame is a Game that implements the rules of Chess.
+ * @see https://en.wikipedia.org/wiki/Chess
+ */
+export default class ChessGame extends Game<ChessGameState, ChessMove> {
+  public constructor() {
+    super({
+      moves: [],
+      status: 'WAITING_TO_START',
+    });
+  }
+
+  /*
+   * Makes API request to obtain best possible move for current game state and returns the move
+   *
+   * @param move The move to apply to the game
+   * @throws InvalidParametersError if the move is invalid (with specific message noted above)
+   */
+  protected _bestMove(): GameMove<ChessMove> {}
+
+  /**
+   * Returns all possible moves for the piece at the given rank and file 
+   * 
+   * @param rank The rank position
+   * @param file The file position
+   */
+  protected _possibleMoves(rank: ChessRankPosition, file: ChessFilePosition;): void {}
+
+
+  /*
+   * Applies a player's move to the game.
+   * Uses the player's ID to determine which game pieces (white vs black) they are using (ignores move.gamePiece)
+   * Validates the move before applying it. If the move is invalid, throws an InvalidParametersError with
+   * the error message specified below.
+   * A move is invalid if:
+   *   ... comment to be updated after implementation
+   *
+   * If the move is valid, applies the move to the game and updates the game state.
+   *
+   * If the move ends the game, updates the game's state.
+   * If the move results in a tie, updates the game's state to set the status to OVER and sets winner to undefined.
+   * If the move results in a win, updates the game's state to set the status to OVER and sets the winner to the player who made the move.
+   * A player wins if they check the the opposing king and the king has no way to get out of check
+   * A stalemate result in a tie
+   *
+   * @param move The move to apply to the game
+   * @throws InvalidParametersError if the move is invalid (with specific message noted above)
+   */
+  public applyMove(move: GameMove<ChessMove>): void {}
+
+  /**
+   * Adds a player to the game.
+   * Updates the game's state to reflect the new player.
+   * If the game is now full (has two players), updates the game's state to set the status to IN_PROGRESS.
+   *
+   * @param player The player to join the game
+   * @throws InvalidParametersError if the player is already in the game (PLAYER_ALREADY_IN_GAME_MESSAGE)
+   *  or the game is full (GAME_FULL_MESSAGE)
+   */
+  public _join(player: Player): void {}
+
+  /**
+   * Removes a player from the game.
+   * Updates the game's state to reflect the player leaving.
+   * If the game has two players in it at the time of call to this method,
+   *   updates the game's status to OVER and sets the winner to the other player.
+   * If the game does not yet have two players in it at the time of call to this method,
+   *   updates the game's status to WAITING_TO_START.
+   *
+   * @param player The player to remove from the game
+   * @throws InvalidParametersError if the player is not in the game (PLAYER_NOT_IN_GAME_MESSAGE)
+   */
+  protected _leave(player: Player): void {}
+}
---
+++ townService/src/town/games/ChessGameArea.ts
@@ -0,0 +1,50 @@
+import Player from '../../lib/Player';
+import {
+  InteractableCommand,
+  InteractableCommandReturnType,
+  InteractableType,
+} from '../../types/CoveyTownSocket';
+import GameArea from './GameArea';
+import ChessGame from './ChessGame';
+
+/**
+ * A ChessGameArea is a GameArea that hosts a TicTacToeGame.
+ * @see ChessGame
+ * @see GameArea
+ */
+export default class ChessGameArea extends GameArea<ChessGame> {
+  private _type: InteractableType = 'ChessArea';
+
+  protected getType(): InteractableType {
+    return this._type;
+  }
+
+  /**
+   * Handle a command from a player in this game area.
+   * Supported commands:
+   * - JoinGame (joins the game `this._game`, or creates a new one if none is in progress)
+   * - GameMove (applies a move to the game)
+   * - LeaveGame (leaves the game)
+   *
+   * If the command ended the game, records the outcome in this._history
+   * If the command is successful (does not throw an error), calls this._emitAreaChanged (necessary
+   *  to notify any listeners of a state update, including any change to history)
+   * If the command is unsuccessful (throws an error), the error is propagated to the caller
+   *
+   * @see InteractableCommand
+   *
+   * @param command command to handle
+   * @param player player making the request
+   * @returns response to the command, @see InteractableCommandResponse
+   * @throws InvalidParametersError if the command is not supported or is invalid. Invalid commands:
+   *  - LeaveGame and GameMove: No game in progress (GAME_NOT_IN_PROGRESS_MESSAGE),
+   *        or gameID does not match the game in progress (GAME_ID_MISSMATCH_MESSAGE)
+   *  - Any command besides LeaveGame, GameMove and JoinGame: INVALID_COMMAND_MESSAGE
+   */
+  public handleCommand<CommandType extends InteractableCommand>(
+    command: CommandType,
+    player: Player,
+  ): InteractableCommandReturnType<CommandType> {
+    throw new Error('Unimplemented - remove this once you start to implement this method');
+  }
+}
