18867c670e114f9a447db790eea3cee8c4a85c0e
sivaanand13 <99034823+sivaanand13@users.noreply.github.com>
2023-11-30 01:15:44 -0500

    Made necessary changes to make interactable chess location

--- frontend/src/components/Town/interactables/Chess/ChessArea.tsx
+++ frontend/src/components/Town/interactables/Chess/ChessArea.tsx
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState, useEffect, useCallback } from 'react';
 import { Modal, ModalOverlay, ModalContent, ModalCloseButton, ModalHeader } from '@chakra-ui/react';
 import Leaderboard from './Leaderboard';
 import useTownController from '../../../../hooks/useTownController';
-import { GameResult } from '../../../../types/CoveyTownSocket';
+import { GameResult, InteractableID } from '../../../../types/CoveyTownSocket';
 import { generateDummyChessResults } from './DummyResults';
+import { useInteractable } from '../../../../classes/TownController';
+import GameAreaInteractable from '../GameArea';
 
-function ChessArea(): JSX.Element {
+function ChessArea({ interactableID }: { interactableID: InteractableID }): JSX.Element {
   const [chessResults, setChessResults] = useState<GameResult[]>(generateDummyChessResults());
   const townController = useTownController();
 
@@ -17,15 +20,40 @@ function ChessArea(): JSX.Element {
 
   // Modal to display the leaderboard
   return (
-    <Modal isOpen={true} onClose={() => townController?.disconnect()} closeOnOverlayClick={false}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Chess Game Leaderboard</ModalHeader>
-        <ModalCloseButton />
-        <Leaderboard results={chessResults} />
-      </ModalContent>
-    </Modal>
+    <>
+      <Leaderboard results={chessResults} />
+    </>
   );
 }
 
-export default ChessArea;
+/**
+ * A wrapper component for the ChessArea component.
+ * Determines if the player is currently in a chess area on the map, and if so,
+ * renders the ChessArea component in a modal.
+ *
+ */
+export default function ChessAreaWrapper(): JSX.Element {
+  const gameArea = useInteractable<GameAreaInteractable>('gameArea');
+  const townController = useTownController();
+  const closeModal = useCallback(() => {
+    if (gameArea) {
+      townController.interactEnd(gameArea);
+      const controller = townController.getGameAreaController(gameArea);
+      controller.leaveGame();
+    }
+  }, [townController, gameArea]);
+
+  if (gameArea) {
+    return (
+      <Modal isOpen={true} onClose={closeModal} closeOnOverlayClick={false}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{gameArea.name}</ModalHeader>
+          <ModalCloseButton />
+          <ChessArea interactableID={gameArea.name} />;
+        </ModalContent>
+      </Modal>
+    );
+  }
+  return <></>;
+}
--- frontend/src/types/TypeUtils.ts
+++ frontend/src/types/TypeUtils.ts
@@ -21,5 +21,5 @@ export function isViewingArea(interactable: Interactable): interactable is Viewi
 }
 
 export function isChessArea(interactable: Interactable): interactable is GameArea<ChessGameState> {
-  return interactable.type === 'ChesseArea';
+  return 'history' in interactable;
 }
